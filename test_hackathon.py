259161f1fa91ecf70f4c6a16109127ea9e838f55
xup65k6t6 <xup65k6t6@gmail.com>
2022-11-05 15:53:34 -0700

    add 3 files and revise README

--- test_hackathon.py
+++ test_hackathon.py
@@ -1,7 +1,7 @@
 #%%
 import pandas as pd
-
-navigation_events = pd.read_csv("data/navigation_events.csv")
+pd.set_option('display.max_columns', None)
+navigation_events = pd.read_csv("data/navigation_events.csv", parse_dates=['event_time', 'event__eventTime'])
 assignments = pd.read_csv("data/additional/assignments.csv")
 discussion_topics = pd.read_csv("data/additional/discussion_topics.csv")
 discussions = pd.read_csv("data/additional/discussions.csv")
@@ -11,3 +11,4 @@ gradebook = pd.read_csv("data/additional/gradebook.csv")
 module_items = pd.read_csv("data/additional/module_items.csv") 
 pages = pd.read_csv("data/additional/pages.csv")
 # %%
+discussions['discussion_topic_title'].value_counts().sort_index()

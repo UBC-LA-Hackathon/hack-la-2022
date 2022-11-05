be687bc0db91635fba429e067b4d936319b77ec4
xericf <xericf@gmail.com>
2022-11-05 10:25:37 -0700

    added graph function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import Data from './Components/Data'
+import Data from './Components/Data';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Data/>
-      
-    </div>
-  );
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries} from 'react-vis';
+
+class App extends Component {
+  render() {
+    const data = [
+      {x: 0, y: 8},
+      {x: 1, y: 5},
+      {x: 2, y: 4},
+      {x: 3, y: 9},
+      {x: 4, y: 1},
+      {x: 5, y: 7},
+      {x: 6, y: 6},
+      {x: 7, y: 3},
+      {x: 8, y: 2},
+      {x: 9, y: 0}
+    ];
+    return (
+      <div className="App">
+        <XYPlot height={300} width={300}>
+          <LineSeries data={data} />
+        </XYPlot>
+
+        <Data/>
+      </div>
+    );
+  }
 }
+
 
 export default App;

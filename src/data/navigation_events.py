f2659398d21c89676f64737c78f308af61a74b7f
Ty Andrews <ty.elgin.andrews@gmail.com>
2022-11-05 10:21:19 -0700

    first pre-process scripting done

--- src/data/navigation_events.py
+++ src/data/navigation_events.py
@@ -14,9 +14,30 @@ def load_events_data(filepath):
 
 def preprocess_events_data(events_df):
 
-    # drop event__eventTime
+    clean_events_df = events_df.copy(deep=True)
+
+    # drop event__eventTime, type, event__eventType, event__action, event__object_extensions_http_method, event__edApp_type, event__object_id_type
+    drop_columns = [
+        "event__eventTime",
+        "type",
+        "event__type",
+        "event__action",
+        "event__object_extensions_http_method",
+        "event__edApp_type",
+        "event__object_id_type",
+    ]
+
+    clean_events_df.drop(columns=drop_columns)
+
     # membership role remove [""] symbols
-    # remove type column, and event__eventType
+    clean_events_df["membership_role"] = (
+        clean_events_df.membership_role.str.replace('"', "", regex=True)
+        .str.replace("[", "", regex=True)
+        .str.replace("]", "", regex=True)
+    )
+
+    clean_events_df["event_time"] = pd.to_datetime(clean_events_df.event_time)
+
     # deal with null membership role?
 
-    return None
+    return clean_events_df

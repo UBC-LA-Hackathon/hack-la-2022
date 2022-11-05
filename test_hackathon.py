1c5b268b83dc3e72d4c88d0d96827543aef23589
xup65k6t6 <xup65k6t6@gmail.com>
2022-11-05 09:01:45 -0700

    testing

---
+++ test_hackathon.py
@@ -0,0 +1,13 @@
+#%%
+import pandas as pd
+
+navigation_events = pd.read_csv("data/navigation_events.csv")
+assignments = pd.read_csv("data/additional/assignments.csv")
+discussion_topics = pd.read_csv("data/additional/discussion_topics.csv")
+discussions = pd.read_csv("data/additional/discussions.csv")
+enrollments = pd.read_csv("data/additional/enrollments.csv") 
+files = pd.read_csv("data/additional/files.csv") 
+gradebook = pd.read_csv("data/additional/gradebook.csv") 
+module_items = pd.read_csv("data/additional/module_items.csv") 
+pages = pd.read_csv("data/additional/pages.csv")
+# %%

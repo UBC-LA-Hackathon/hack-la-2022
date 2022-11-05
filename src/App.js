db54647a5ea4b41a48d82fd52d7206e006754298
Kevin Hu <kevinh64@student.ubc.ca>
2022-11-05 10:45:50 -0700

    team intro + pictures pog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Data from './Components/Data'
+import Squad from './Components/Squad'
 
 function App() {
   return (
     <div className="App">
+      <Squad/>
       <Data/>
       
     </div>
---
+++ src/Components/Squad.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Stuart from '../imgs/stc.jpg'
+import Kevin from '../imgs/kevin hu.jfif'
+import Allan from '../imgs/allan xing.jfif'
+import Eric from '../imgs/eric fu.jfif'
+
+const Squad = () => {
+    return (
+        <div>
+            <h1>Team Introduction!</h1>
+      <img src={Stuart} width="200" height="200"></img>
+      <img src={Kevin} width="200" height="200"></img>
+      <img src={Allan} width="200" height="200"></img>
+      <img src={Eric} width="200" height="200"></img>
+        </div>
+    )
+}
+
+
+export default Squad
